36e72b8f19ae13514088f63dd7c31603eb202633
NEHA <ayushichoudhary0016@gmail.com>
2023-11-01 13:24:03 +0530

    UIEnhancements

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("") 
-  const [copyMessage, setCopyMessage] = useState(''); 
-  
+  const [copyMessage, setCopyMessage] = useState('Copy'); 
+  const [flag, setFlag] = useState('good');
+  const [flagColor, setFlagColor] = useState('#F59E0B');  
+  const [copyChangeBorder,setCopyChangeBorder] = useState('border-super-light-grey')
+  const [copyChangeBg,setCopyChangeBg] = useState('bg-light-grey')
 //useCallback(function , [array of dependencies])
 //this function is used to store some reusable items of a function in cache. thus used for memoization
   const  passwordGenerator =  useCallback(() =>{
@@ -22,7 +25,6 @@ function App() {
     setPassword(pass)
 
   } ,[length,numberAllowed,charAllowed,setPassword]);
-
   
   useEffect(()=> {
     passwordGenerator();
@@ -34,53 +36,83 @@ function App() {
   //without this also it copies but user won't know what area is copied
 
   const copyPasswordToClipboard=useCallback(() => {
-    passwordRef.current?.select();
     window.navigator.clipboard.writeText(password);
-    setCopyMessage('Copied!');
+    setCopyMessage('✅');
+    setCopyChangeBorder('border-bright-blue');
+    setCopyChangeBg('bg-bright-blue');
     // Clear the message after 1 second
     setTimeout(() => {
-      setCopyMessage('');
-    }, 1000);
+      setCopyMessage('Copy');
+      setCopyChangeBorder('border-super-light-grey'); 
+      setCopyChangeBg('bg-light-grey');
+    }, 1500);
   },[password])
+
+  useEffect(() => {
+    if (length > 15) {
+      setFlagColor('green');
+    } else if (length >= 8) {
+      setFlagColor('#F59E0B');
+    } 
+    else{
+      setFlagColor('red');
+    }
+  }, [length]);
+
+  useEffect(() => {
+    if (length > 15) {
+      setFlag('strong');
+    } else if (length >= 8) {
+      setFlag('good');
+    } 
+    else{
+      setFlag('weak');
+    }
+  }, [length]);
+
 
   return (
     <>
-    <h1 className='text-white font-bold text-4xl mt-20 text-center'style={{color:'#C26291'}}>PassCipher</h1>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-2 my-8 mt-10 bg-gray-700' style={{backgroundColor:'#4a5b6a',color:'#728495'}}>
-    <h1 className='text-center text-white my-2'> Generator a random password</h1>        
-    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+    <h1 className='text-white font-bold text-4xl mt-20 text-center'>PassCipher</h1>
+    <div className='w-full max-w-lg mx-auto shadow-md rounded-lg px-10 py-2 my-8 mt-10 bg-gray-700 text-greyish-white' style={{backgroundColor:'#202229'}}>
+    <h1 className='text-center text-white my-5'> Generate a random password</h1>        
+    <div className={`flex shadow rounded-lg overflow-hidden mb-5 border ${copyChangeBorder}`}>
       <input 
       type='text'
       value={password}
-      className='outline-none w-full py-1 px-3'
+      className={`outline-none w-full py-1 px-3 ${copyChangeBg}`}
       placeholder='password'
       readOnly
       ref={passwordRef}
       >
       </input>
-      <button className='outline-none text-white px-3 py-0.5 shrink-0 '
-      onClick={copyPasswordToClipboard} style={{backgroundColor:'#131E88'}}>
-        Copy</button>
+      <div className={`${copyChangeBg} outline-none py-1 px-3`}>
+        <div className='outline-none py-1 px-3 my-1 rounded-lg text-white' style={{backgroundColor:flagColor}}>{flag}</div>
+        </div>
+      <button className='w-20 outline-none text-white px-3 py-0.5 shrink-0 bg-main-blue transition-all duration-300 hover:bg-blue-800'
+      onClick={copyPasswordToClipboard}>
+        {copyMessage}</button>
  
     </div>
     
 
-    <div className='text-sm gap-x-2'>
+    <div>
     <div className='items-centre gap-x-1'>
     <label>Length: {length}</label>
     <br/>
       <input
       type='range'
-      min={8}
+      min={2}
       max={30}
       value={length}
-      className='cursor-pointer mt-1 w-full'
+      className=' cursor-pointer mt-1 w-full h-1 mb-2 mt-1'
       //onClick and onChange can never hold return value of function so we give it a function () itself
       onChange={(e)=>{setLength(e.target.value)}}
+
       />
   
     </div>
-    <div className='flex items-center gap-x-1'>
+    <div className='flex items-center gap-x-1  mb-2'>
       <input
       type='checkbox'
       defaultChecked={numberAllowed}
@@ -102,7 +134,6 @@ function App() {
       <label>Characters</label>
     </div>
     </div>
-    {copyMessage && <p className="text-green-500 text-center text-xs">{copyMessage}</p>}
 
     </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         '50': '0.5',
         '75': '0.75',
         '90': '0.9',
+      },
+      backgroundColor:{
+        'light-grey' : '#2B2D34',
+        'bright-blue' :'#202B3C', 
+        'main-blue' : '#1F5CF8',
+      },
+      borderColor:{
+        'super-light-grey' :'#3D3F46',
+        'bright-blue':'#1F5399',
+      },
+      textColor:{
+        'greyish-white':'#A8A8AB',
       }
     },
   },
